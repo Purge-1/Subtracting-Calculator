e7c22f5f54322856d3efbf85d25132f808f8ce3e
Purge-1 <80547719+Purge-1@users.noreply.github.com>
2021-10-10 01:02:14 -0700

    Add files via upload

---
+++ subtract.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("title Subtract from 100")
+
+hello = int(input('Subtract from 100 -> '))
+
+answer = 100 - hello
+
+print(f"You're subtracting 100 from {hello} and the answer is {answer}\nMade by Purge-1")
+
+os.system("pause")
